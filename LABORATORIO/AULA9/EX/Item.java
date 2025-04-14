63b41c81aabb7c96b38b5ccdd748143c8ba25e20
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2025-04-14 20:02:48 -0300

    Create Item.java

---
+++ LABORATORIO/AULA9/EX/Item.java
@@ -0,0 +1,17 @@
+public class Item {
+    private int valor;
+    private String nome;
+
+    public Item(int valor, String nome) {
+        this.valor = valor;
+        this.nome = nome;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
